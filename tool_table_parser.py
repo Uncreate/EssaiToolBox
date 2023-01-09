23154e5e45190e69ba98a32c5b7b1013fe45696d
Adam Riggs <ajriggs829@gmail.com>
2023-01-09 14:10:40 -0700

    Updated with home

--- tool_table_parser.py
+++ tool_table_parser.py
@@ -1,10 +1,7 @@
-import configparser
-import datetime
 import os
 import re
-
+import configparser
 import matplotlib.pyplot as plt
-
 
 def load_config():
     """Load the configuration file."""
@@ -12,50 +9,36 @@ def load_config():
     config.read('config.ini')
     return config
 
+def get_directory(config):
+    """Get the directory to search from the configuration file."""
+    return config['PATHS']['tool_t_path']
+
+def get_graph_storage(config):
+    return config['PATHS']['graph_storage']
+
 def extract_data(filename):
-    """Extract the tool names, DR values, DL values, and Last_used dates from the file, ignoring rows where both DR and DL values are zero and tools that have not been used in the last six months."""
+    """Extract the tool names, DR values, and DL values from the file, ignoring rows where both values are zero."""
     with open(filename, 'r') as f:
         lines = f.readlines()[2:]
-    pattern = re.compile(r'^(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)')
+    pattern = re.compile(r'^(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)')
     dr_names = []
     dl_names = []
     drs = []
     dls = []
-    last_used_dates = []
     for line in lines:
         if match := pattern.match(line):
             name = match[1]
             dr = float(match[5])
             dl = float(match[6])
-            last_used = datetime.datetime.strptime(match[22], '%Y.%m.%d %H:%M')
-            now = datetime.datetime.now()
-            diff = now - last_used
-            if abs(dr) != 0 and diff.days < 180:
+            if abs(dr) != 0:
                 dr_names.append(name)
                 drs.append(dr)
-                
-            elif abs(dl) != 0 and diff.days < 180:
+            elif abs(dl) != 0:
                 dl_names.append(name)
-                dls.append(dl)
-                
-    return dr_names, dl_names, drs, dls
+                dls.append(dl)    
+    return dr_names,dl_names, drs, dls
 
-def process_files(config):
-    # Get the directory to search
-    base_dir = config['PATHS']['tool_t_path']
-    # Get the storage location for the graphs
-    graph_storage = config['PATHS']['graph_storage']
-
-    # Search for T files in the subdirectories of base_dir
-    for root, dirs, files in os.walk(base_dir):
-        for file in files:
-            if file.endswith('.T'):
-                full_path = os.path.join(root, file)
-                print(full_path)
-                dr_names, dl_names, drs, dls = extract_data(full_path)
-                plot_data(graph_storage, file, dr_names, dl_names, drs, dls)
-
-def plot_data(graph_storage, file, dr_names, dl_names, drs, dls):
+def plot_data(graph_storage, filename, dr_names, dl_names, drs, dls):
     # sourcery skip: extract-duplicate-method
     # Create a figure and axis
     fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(20, 9), layout='constrained')
@@ -76,14 +59,20 @@ def plot_data(graph_storage, file, dr_names, dl_names, drs, dls):
 
     # Tighten layout and show plot
     plt.xticks(rotation = 45)
-    plt.suptitle(file)
-    fig.savefig(os.path.join(graph_storage, f"{file}.png"))
+    plt.suptitle(filename)
+    fig.savefig(os.path.join(graph_storage, f"{filename}.png"))
 
     
     plt.close()
 def main():
     config = load_config()
-    process_files(config)
+    directory = get_directory(config)
+    graph_storage = get_graph_storage(config)
+    filenames = os.listdir(directory)
+    for filename in filenames:
+        full_path = os.path.join(directory, filename)
+        dr_names, dl_names, drs, dls = extract_data(full_path)
+        plot_data(graph_storage, filename, dr_names, dl_names, drs, dls)
 
 if __name__ == '__main__':
     main()
